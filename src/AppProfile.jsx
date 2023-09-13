99d854e182e02d5c12cacaecffcf622c061f7168
Ella <38400022+jglorys@users.noreply.github.com>
2023-09-13 16:08:05 +0900

    4.11 새로운 개발자 태그 만들기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -13,6 +13,7 @@ function AppProfile() {
         image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
         name='Ella'
         title='백엔드 개발자'
+        isNew={true}
       />
 
     </>
