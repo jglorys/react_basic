c5c9cf0c0c7db498ceb1840a254149b43637dad2
Ella <38400022+jglorys@users.noreply.github.com>
2023-10-17 16:17:53 +0900

    [4.17] 카운터 상태 끌어올리기

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
+export default function Counter({total, onClick}) {
+    const [count, setCount] = useState(0); 
+    //초기값을 0으로 셋팅했고, useState호출하면 배열이 전달됨
+    // 0 : 변수, 1: 업데이트 하는 함수
     return (
         <div className='counter'>
-            <span className='number'>{count}</span>
-            <button className='button' onClick={()=>{
-                setCount(count + 1);
+            <p className='number'>{count} 
+                <span className='total'>/{total}</span>
+            </p>
+            <button 
+            className='button' 
+            onClick={()=>{
+                setCount((prev) => prev+1);
+                onClick();
             }}>Add +</button>
         </div>
     );
