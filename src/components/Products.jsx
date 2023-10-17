772f233d89e3a9771078bc14dce4b016e42712f3
Ella <38400022+jglorys@users.noreply.github.com>
2023-10-17 17:49:39 +0900

    [4.20] useEffect 제대로 사용하기

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
     const [products, setProducts] = useState([]);
-
+    const [checked, setChecked] = useState(false);
+    const handleChange = () => setChecked((prev) => !prev);
     useEffect(() => {
-        fetch('data/products.json')
+        fetch(`data/${checked ? 'sale_' : ''}products.json`)
         .then((res) => res.json())
         .then((data) => {
             console.log('뜨끈한 데이터를 네트워크에서 받아옴');
@@ -14,9 +15,11 @@ export default function Products() {
         return () => {
             console.log('🪷깨끗하게 청소하는 일들을 합니다');
         }
-    }, []);
+    },[checked]);
     return (
         <div>
+            <input id='checkbox' type='checkbox' value={checked} onChange={handleChange}></input>
+            <label htmlFor='checkbox'>Show Only Hot Sale!</label>
             <ul>
                 {products.map( (product) => (
                     <li key={product.id}>
@@ -27,9 +30,9 @@ export default function Products() {
                     </li>
                 ))}
             </ul>
-            <button onClick={() => setCount((prev) => prev + 1)}>
+            {/* <button onClick={() => setCount((prev) => prev + 1)}>
                 {count}
-            </button>
+            </button> */}
         </div>
     );
 }
