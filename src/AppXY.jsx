bb1c73e40dcdb388f3bf6bdf3173b05222a9dde1
Ella <38400022+jglorys@users.noreply.github.com>
2023-10-18 17:21:50 +0900

    [5.4] 마우스 따라가기 - 개선하기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import './AppXY.css';
+import { click } from '@testing-library/user-event/dist/click';
 
 export default function AppXY() {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
+    const [position, setPosition] = useState({x:0, y:0, z:0});
     return (
         <div className='container' onPointerMove={(e) => {
-            console.log(e.clientX, e.clientY);
-            setX(e.clientX);
-            setY(e.clientY);
+            // console.log(e.clientX, e.clientY);
+            // setPosition({x: e.clientX, y: e.clientY});
+            
+            // 만약 수평으로만 이동이 가능하다면?
+            // setPosition((prev) => ({x: e.clientX, y: prev.y}));
+
+            setPosition((prev) => ({...prev, x: e.clientX}));
         }}>
-            <div className='pointer' style={{transform: `translate(${x}px, ${y}px)`}}></div>
+            <div className='pointer' style={{transform: `translate(${position.x}px, ${position.y}px)`}}>
+                
+            </div>
         </div>
     );
 }
